80e17f5df4e34aecceb4fd453f2bbdbeb47ab519
Mahtab sheikh <119559082+Mahtab12221727@users.noreply.github.com>
2025-10-27 19:35:49 +0000

    datatypes

---
+++ 01_Basic/01_datatype.js
@@ -0,0 +1,15 @@
+//primitive data
+//number
+//string
+//boolean
+//bigint
+//null
+//undefined
+//sysmbil=unique
+let age=18;
+let name="hitesh";
+//non primitive
+//objext
+console.log(typeof"hetesh");
+console.log("age",age);
+console.log("name",name);
