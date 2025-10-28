8bdce67a058a7440c252ec7d7d0fc4bcf448d2e1
Mahtab sheikh <119559082+Mahtab12221727@users.noreply.github.com>
2025-10-28 19:11:19 +0000

    conversion

---
+++ 01_Basic/02_conversation.js
@@ -0,0 +1,13 @@
+let num=33;
+//let a="33";
+//console.log(num);
+//console.log(typeof num);
+//console.log(typeof a);
+//string to number
+//console.log(String(num));
+//let b=true;
+l//et f=33.62;
+//console.log(typeof(f));
+//number. to string 
+let s=String(num);
+comsole.log(s);
